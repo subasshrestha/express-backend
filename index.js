1aea254493ed06f03d51b5d972f89a935747dfa4
subas shrestha <subasshrestha513@gmail.com>
2022-06-24 09:08:08 +0545

    Adds new features

--- index.js
+++ index.js
@@ -3,29 +3,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const RedisStore = require('rate-limit-redis');
 
 const routeManager = require('./routes');
 const logger = require('./helpers/logger');
 const { handleError, ErrorHandler } = require('./helpers/errorHandler');
+const { corsOptions } = require('./helpers/cors');
+const { opts } = require('./helpers/redisConnection');
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 10,
+  delayMs: 5000,
+  standardHeaders: true,
+  legacyHeaders: false,
+  store: new RedisStore({
+    client: opts.createClient(),
+    expiry: 10 * 60,
+  }),
+});
 
-const corsAllowList = ['*'];
-const corsOptionsDelegate = (req, callback) => {
-  let corsOptions;
-  if (corsAllowList.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true };
-  } else {
-    corsOptions = { origin: false };
-  }
-
-  callback(null, corsOptions);
-};
-
-app.use(cors(corsOptionsDelegate));
+app.enable('trust proxy');
+app.use(cors(corsOptions));
+app.use(limiter);
 
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => logger.info('Database Connected'))
+  .then(() => logger.info('MongoDB:: Database Connected'))
   .catch((err) => logger.error(err));
 
 app.use(express.json());
